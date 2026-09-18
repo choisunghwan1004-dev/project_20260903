e6527633c6da4312b34b9ccbf17fc4f12fb0f877
choisunghwan1004-dev <choisunghwan1004@gmail.com>
2026-09-18 15:16:29 +0900

    나의 첫 소스코드 올리기

---
+++ gugudan.js
@@ -0,0 +1,9 @@
+<script>
+       for ( j = 1; j < 10; j++) {
+        console.log(j + "단");
+         for (let i=1; i<100; i++) 
+          console.log( index + "*" + i + "=" + index*i)
+       }
+
+
+</script>
