fee486e8399aaa766bd78125d12e06cc53e4ec0b
choisunghwan1004-dev <choisunghwan1004@gmail.com>
2026-09-18 15:44:36 +0900

    Refactor JavaScript functions and clean up HTML structure

--- gugudan.js
+++ gugudan.js
@@ -1,9 +1,7 @@
-<script>
+
        for ( j = 1; j < 10; j++) {
         console.log(j + "단");
          for (let i=1; i<100; i++) 
-          console.log( index + "*" + i + "=" + index*i)
+          console.log( j + "*" + i + "=" + j*i)
        }
 
-
-</script>
